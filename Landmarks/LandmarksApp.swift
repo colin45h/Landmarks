cf181db1ebce4dd4c56d52c13e3a081857623eac
Colin Hehn <colin45h@yahoo.com>
2021-05-24 11:43:47 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Colin Hehn on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
